cf857cf9765d6fab21f8bc1d6ea9807544e28db6
Christopher Brown <cb@skeptomai.com>
2015-11-27 13:15:40 -0800

    make transport and request static use try-with-resources

--- src/main/java/com/skeptomai/MP3Fetcher.java
+++ src/main/java/com/skeptomai/MP3Fetcher.java
@@ -4,6 +4,7 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.util.ByteStreams;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -18,12 +19,13 @@ public class MP3Fetcher implements Callable<String> {
     private static final String outputDir = System.getProperty("user.dir");
     private static final Logger log = Logger.getLogger(MP3Fetcher.class.getName());
 
-    private final NetHttpTransport netHttpTransport;
+    private static final NetHttpTransport netHttpTransport = new NetHttpTransport();
+    private static final HttpRequestFactory requestFactory =
+            netHttpTransport.createRequestFactory();
 
     private String urlToFetch;
 
     public MP3Fetcher(String url) {
-        netHttpTransport = new NetHttpTransport();
         urlToFetch = url;
     }
 
@@ -43,28 +45,25 @@ public class MP3Fetcher implements Callable<String> {
             log.info("Looks like " + basename + " is already there.. Skipping");
         } else {
             GenericUrl url = new GenericUrl(urlToFetch);
-            HttpRequestFactory requestFactory =
-                    netHttpTransport.createRequestFactory();
 
             HttpRequest request = requestFactory.buildGetRequest(url);
-            InputStream is = request.execute().getContent();
+
             File f = File.createTempFile("mp3fetcher", ".mp3");
             log.info(f.getAbsolutePath());
-            //f.deleteOnExit();
 
-            FileOutputStream outputStream = new FileOutputStream(f);
-            byte[] buffer = new byte[4096];
-            int len;
-            while ((len = is.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, len);
+            try(InputStream is = request.execute().getContent();
+                FileOutputStream outputStream = new FileOutputStream(f)) {
+                //f.deleteOnExit();
+                ByteStreams.copy(is, outputStream);
+
+                Path pathTempMP3 = f.toPath();
+                log.info("Moving " + pathTempMP3.toString() + " to " + pathFinalMP3.toString());
+                Files.move(pathTempMP3, pathFinalMP3);
             }
-            outputStream.close();
-            Path pathTempMP3 = f.toPath();
 
 
-            log.info("Moving " + pathTempMP3.toString() + " to " + pathFinalMP3.toString());
-            Files.move(pathTempMP3, pathFinalMP3);
         }
+
         return pathFinalMP3.toString();
     }
 
