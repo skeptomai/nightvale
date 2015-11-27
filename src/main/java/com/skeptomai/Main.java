de226bf5b1699f359cbda7198b05a6531f326920
Christopher Brown <cb@skeptomai.com>
2015-11-27 10:06:11 -0800

    trying listenable future

--- src/main/java/com/skeptomai/Main.java
+++ src/main/java/com/skeptomai/Main.java
@@ -1,5 +1,6 @@
 package com.skeptomai;
 
+import com.google.common.util.concurrent.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -24,16 +25,34 @@ public class Main {
 
     private static void fetchMP3s(List<String> mp3sToFetch) {
         List<Future<String>> fl = new ArrayList<>();
+
         final ExecutorService pool = Executors.newFixedThreadPool(5);
-        ExecutorCompletionService executor = new ExecutorCompletionService(pool);
+
+        ListeningExecutorService executor = MoreExecutors
+                .listeningDecorator(pool);
+
         for (String mp3ToFetch : mp3sToFetch) {
             log.info(mp3ToFetch);
-            fl.add(executor.submit(new MP3Fetcher(mp3ToFetch)));
+
+            ListenableFuture<String> lf = executor.submit(new MP3Fetcher(mp3ToFetch));
+            fl.add(lf);
+
+            Futures.addCallback(lf, new FutureCallback<String>() {
+                public void onSuccess(String result) {
+                    System.out.println("Gettin' called back: " + result);
+                }
+
+                public void onFailure(Throwable thrown) {
+                    System.out.println("Shit! Failed: " + thrown.getMessage());
+                }
+            });
+
         }
 
         for (Future<String> fi : fl){
             try {
-                fi.get();
+                String result = fi.get();
+                System.out.println("actually made it past get..: " + result);
             } catch (ExecutionException | InterruptedException ee) {
                 log.severe("Interrupted future.  World weeps");
             }
